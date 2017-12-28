6f55e9a91f91a86e88b8c5249665244f8a139b20
Elena Shulbaeva <kotlova.elena@gmail.com>
2017-12-28 20:06:26 +0700

    Fill About page

---
+++ Assets/Member.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Member : MonoBehaviour {
+
+    public string Name;
+    public Sprite Picture;
+
+	// Use this for initialization
+	void Start () {
+        Image pictureChild = GetComponentInChildren<Image>();
+        if (pictureChild != null && Picture != null)
+        {
+            pictureChild.sprite = Picture;
+        }
+
+        Text nameChild = GetComponentInChildren<Text>();
+        if (nameChild != null && Name != null)
+        {
+            nameChild.text = Name;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
